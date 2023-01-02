8e15121e17b237644befe84b0ac19191f12367f1
pPlanello <planello96@gmail.com>
2023-01-02 12:30:07 +0100

    Create filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,19 @@
-
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../Shared/Card';
 import './Expenses.css';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
 
 const Expenses = (props) => {
+
+    const [filteredYear, setFilteredYear] = useState('');
+
+    const selectListener = (selectYear) => {
+        setFilteredYear(selectYear);
+    }
     return (
         <Card className='expenses'>
+            <ExpensesFilter onSelectListener={selectListener}/>
             <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
             <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
             <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
