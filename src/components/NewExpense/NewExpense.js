b160964f90ff9bb09d23ef69d956369747fc8be1
pPlanello <planello96@gmail.com>
2023-01-02 13:52:19 +0100

    Implement form of expenses

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewExpense.css';
 import ExpenseFrom from './ExpenseForm';
 
 
 const NewExpense = (props) => {
+    const [showExpenseForm, setShowExpenseForm] = useState(false);
     const saveNewExpenseListener = (enteredNewExpense) => {
         const newExpense = {
             ...enteredNewExpense,
             id: Math.random().toString()
         };
 
-
         props.onAddNewExpense(newExpense);
     }
+
+    const newExpenseListener = () => {
+        setShowExpenseForm(true);
+    }
+
+    const cancelExpenseListener = () => {
+        setShowExpenseForm(false);
+    }
+
     return (
         <div className='new-expense'>
-            <ExpenseFrom onSaveNewExpense={saveNewExpenseListener}/>
+            {!showExpenseForm ? 
+                (<button onClick={newExpenseListener}>Add New Expense</button>) :
+                (<ExpenseFrom onCancelExpense={cancelExpenseListener} onSaveNewExpense={saveNewExpenseListener}/>)}
         </div>
     );
 };
